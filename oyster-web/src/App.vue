75c8988b3973148a360ee990c49e7b5b2213ab64
Parag Naik <naikparag@gmail.com>
2021-07-20 21:07:55 +0530

    setup: vue 3 + bulma

---
+++ oyster-web/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <HelloWorld msg="Oyster"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
