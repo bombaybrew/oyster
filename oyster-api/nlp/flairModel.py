a3cd467cb62014264de45b895923e77dfb7bf29a
Goswami, Manmohan <magoswami@deloitte.com>
2021-08-20 12:03:30 +0530

    Added create and train fucntions for Flair model

--- oyster-api/nlp/flairModel.py
+++ oyster-api/nlp/flairModel.py
@@ -1,47 +1,82 @@
 from flair.data import Corpus
-from flair.datasets import ColumnCorpus
+from flair.datasets import ColumnCorpus, SentenceDataset
 from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings
 from typing import List
 from flair.trainers import ModelTrainer
 from flair.models import SequenceTagger
 from flair.data import Sentence
+from shutil import copyfile
+import os
 
-
-def trainModel():
-    # define columns
-    columns = {0 : 'text', 1 : 'ner'}
-    # directory where the data resides
-    data_folder = 'train'
+def createModel(modelPath):
     # initializing the corpus
-    corpus: Corpus = ColumnCorpus(data_folder, columns,
-                              train_file = 'train.txt',
-                              test_file = 'train.txt',
-                              dev_file = 'train.txt')
+    trainDummy = SentenceDataset([Sentence('')])
+    corpus: Corpus = Corpus(train=trainDummy, test=trainDummy, dev=trainDummy)
+    tagger = getTagger(corpus)
+    file = modelPath+'/blankmodel.pt'                          
+    tagger.save(file)
 
-    # tag to predict
-    tag_type = 'ner'
-    # make tag dictionary from the corpus
-    tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
-
+def getTagger(corpus):
+    tag_dictionary = corpus.make_tag_dictionary(tag_type='ner')                        
     embedding_types : List[TokenEmbeddings] = [
-        WordEmbeddings('glove'),
-        ## other embeddings
-        ]
+    WordEmbeddings('glove'),
+    ## other embeddings
+    ]
     embeddings : StackedEmbeddings = StackedEmbeddings(
-                                 embeddings=embedding_types)
-
-                                
+                             embeddings=embedding_types)
     tagger : SequenceTagger = SequenceTagger(hidden_size=256,
-                                       embeddings=embeddings,
-                                       tag_dictionary=tag_dictionary,
-                                       tag_type=tag_type,
-                                       use_crf=True)
+                               embeddings=embeddings,
+                               tag_dictionary=tag_dictionary,
+                               tag_type='ner',
+                               use_crf=True)
+    return tagger
+
+def getSentences(trainData):
+    sentences = []
+    for data in trainData: 
+        s1 = Sentence(data[0])
+        text = data[0]
+        entities = data[1]['entities']
+        taglist = []
+        for item in entities:
+            subtext = text[item[0]:item[1]].split()
+            startIndex = len(text[:item[0]].split())
+            for i, t in enumerate(subtext):
+                if i > 0:
+                    tag = (i+startIndex, 'I-'+item[2])
+                    taglist.append(tag)
+                else:
+                    tag = (startIndex, 'B-'+item[2])
+                    taglist.append(tag)
+
+        for tag in taglist:
+            s1[tag[0]].add_tag('ner', tag[1])
+        sentences.append(s1)
+    
+    return sentences
+
+def trainModel(path, modelfile, trainData):
+    ## divide data into train and test
+    sentenceList = getSentences(trainData)
+    trainlen = round(len(sentenceList)*0.80)
+    TRAIN_DATA = SentenceDataset(sentenceList[:trainlen])
+    TEST_DATA = SentenceDataset(sentenceList[trainlen:])
+
+    # initializing the corpus
+    corpus: Corpus = Corpus(train=TRAIN_DATA, test=TEST_DATA)
+
+    if os.path.isfile(path+'/'+modelfile):
+        tagger = SequenceTagger.load(path+'/'+ modelfile)
+    else:
+        tagger = getTagger(corpus)
 
     trainer : ModelTrainer = ModelTrainer(tagger, corpus)
-    trainer.train('resources/taggers',
+    trainer.train(path+'/trained',
               learning_rate=0.1,
               mini_batch_size=32,
-              max_epochs=150)
+              max_epochs=50)
+    ## copy model to main folder
+    copyfile(path+'/trained/final-model.pt', path+'/model.pt')
             
 
 def predict(modelPath, text): 
