e26dd0aa6db5ef6e4d8afbea816b094334810810
Goswami, Manmohan <magoswami@deloitte.com>
2021-08-24 11:14:44 +0530

    add classifier nlp model class
    Modify methods for input train data sets

---
+++ oyster-api/nlp/nlpModel.py
@@ -0,0 +1,20 @@
+
+BLANK_MODEL = 'blankmodel.pt'
+TAG_NER_TYPE = 'ner'
+CLASSIFIER_TYPE = 'textcat'
+
+class NlpModel:
+    def __init__(self, id, fileName="", path=""):
+        self.id = id
+        self.fileName = fileName
+        self.path = path
+    
+    def train(data):
+        pass
+
+    def test(text):
+        pass
+
+    def createAndSave():
+        pass
+    
