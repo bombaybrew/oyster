d19d195d3c972c3d3e7ab60f61f06966dba46b73
Parag Naik <naikparag@gmail.com>
2021-08-26 20:19:37 +0530

    demo: list of models

--- oyster-web/src/views/Demo.vue
+++ oyster-web/src/views/Demo.vue
@@ -4,13 +4,37 @@
       <div class="container">
         <h1>Demo</h1>
         <hr />
-        <model-dropdown
-          v-bind:title="dropdownTitle"
-          v-bind:models="models"
-          v-on:onSelect="onModelSelect"
-        ></model-dropdown>
+
+        <div class="field">
+          <label class="label">Model</label>
+          <div class="control">
+            <model-dropdown
+              v-bind:title="dropdownTitle"
+              v-bind:models="models"
+              v-on:onSelect="onModelSelect"
+            ></model-dropdown>
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">Test Data</label>
+          <div class="control">
+            <textarea class="textarea" placeholder="Test Data"></textarea>
+          </div>
+        </div>
+
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-link">Test</button>
+          </div>
+          <div class="control">
+            <button class="button is-link is-light">Reset</button>
+          </div>
+        </div>
       </div>
     </div>
+
+    <section></section>
   </section>
 </template>
 
@@ -21,28 +45,22 @@ export default {
   name: "Demo",
   data: () => ({
     dropdownTitle: "Select Model",
-    models: [
-      {
-        id: "724b09e6-89b3-4c4d-a0f0-6148e9984929",
-        name: "flair-model",
-        type: "NER",
-        support: "FLAIR",
-        rawDatasetId: "b7e037f2-e999-4a23-bf63-9cbba3a744ae",
-        progress: 0,
-        processedDatasetId: "cadba24f-648a-4e42-bc7b-c4722e979165",
-      },
-      {
-        id: "df59896f-31f3-4f2a-96fc-c7c6b0e9a334",
-        name: "flair-model-classifier",
-        type: "CLASSIFIER",
-        support: "FLAIR",
-        rawDatasetId: "b7e037f2-e999-4a23-bf63-9cbba3a744ae",
-        progress: 0,
-        processedDatasetId: "cb91f09e-38e9-463e-a0c7-dddf77994f20",
-      },
-    ],
+    models: [],
   }),
+  mounted() {
+    this.initDemo();
+  },
+  inject: ["$http"],
   methods: {
+    initDemo: async function() {
+      try {
+        let result = await this.$http.getDemoModels();
+        this.models = result.data.data;
+        console.log(this.models);
+      } catch (e) {
+        console.log(e);
+      }
+    },
     onModelSelect: async function(selected) {
       await this.$nextTick();
       this.dropdownTitle = selected.name;
